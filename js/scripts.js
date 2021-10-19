17d5e3d043b93461ee0f5de49c594fbd218b895b
Jeff Terrell <jeffaterrell@gmail.com>
2021-10-19 14:49:45 -0400

    add buttons for light and dark mode

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $("button#dark").click(function() {
+    $("body").removeClass();
+    $("body").addClass("dark-background");
+    $("body").addClass("light-text");
+  });
+
+  $("button#light").click(function() {
+    $("body").removeClass();
+    $("body").addClass("light-background");
+    $("body").addClass("dark-text");
+  });
+});
